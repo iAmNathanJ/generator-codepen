d254e3447a9118b09c42c76f0772d31177c4da42
Nathan Jacobs <jacobs.123@gmail.com>
2015-07-29 00:07:54 -0400

    add future publish.js

---
+++ app/templates/_publish.js
@@ -0,0 +1,7 @@
+// <header class="publish">
+//   <form action="http://codepen.io/pen/define/" method="post">
+//     {{> codepenData}}
+//     <input type="text" value="${ penTitle }">
+//     <button>Publish</button>
+//   </form>
+// </header>
